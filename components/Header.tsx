4e778a206597047d9d887972f694b8c91a9d81f9
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-09-18 15:09:32 -0700

    import DropdownList

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import DropdownList from "./DropdownList";
 
 const Header = ({ subHeader, title, userImg }: SharedHeaderProps) => {
     return (
-        <header className='header'>
-            <section className='header-container'>
-                <div className='details'>
+        <header className="header">
+            <section className="header-container">
+                <div className="details">
                     {userImg && (
-                        <Image className='rounded-full' src={userImg || '/assets/images/dummy.jpg'} alt='user avatar' width={66} height={66} />
+                        <Image
+                            className="rounded-full"
+                            src={userImg || "/assets/images/dummy.jpg"}
+                            alt="user avatar"
+                            width={66}
+                            height={66}
+                        />
                     )}
 
                     <article>
@@ -18,32 +25,47 @@ const Header = ({ subHeader, title, userImg }: SharedHeaderProps) => {
                 </div>
 
                 <aside>
-                    <Link href='/upload'>
-                        <Image src='/assets/icons/upload.svg' alt='upload' width={16} height={16} />
+                    <Link href="/upload">
+                        <Image
+                            src="/assets/icons/upload.svg"
+                            alt="upload"
+                            width={16}
+                            height={16}
+                        />
                         <span>Upload a video</span>
                     </Link>
 
                     <div className="record">
-                        <button className='primary-btn'>
-                            <Image src='/assets/icons/record.svg' alt='record' width={16} height={16} />
+                        <button className="primary-btn">
+                            <Image
+                                src="/assets/icons/record.svg"
+                                alt="record"
+                                width={16}
+                                height={16}
+                            />
                             <span>Record a video</span>
                         </button>
                     </div>
                 </aside>
             </section>
 
-            <section className='search-filter'>
-                <div className='search'>
+            <section className="search-filter">
+                <div className="search">
                     <input
-                        type='text'
-                        placeholder='Search for videos, tags and folders...'
+                        type="text"
+                        placeholder="Search for videos, tags and folders..."
                     />
-                    <Image src='/assets/icons/search.svg' alt='search' width={16} height={16} />
+                    <Image
+                        src="/assets/icons/search.svg"
+                        alt="search"
+                        width={16}
+                        height={16}
+                    />
                 </div>
-                {'<DropdownList />'}
+                <DropdownList />
             </section>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;

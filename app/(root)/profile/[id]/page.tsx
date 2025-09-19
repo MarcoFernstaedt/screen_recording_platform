5cc3c16227d84bcb1cb6fc81ce0a598c2a81a3f9
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-09-18 20:08:20 -0700

    implament VideoCard

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,14 +1,24 @@
-import Header from '@/components/Header';
-import React from 'react'
+import Header from "@/components/Header";
+import React from "react";
+import VideoCard from '@/components/VideoCard';
+import { dummyCards } from "@/constants";
 
 const page = async ({ params }: ParamsWithSearch) => {
     const { id } = await params;
     return (
-        <div className='wrapper page'>
-            <Header subHeader='marco@email.com' title='Marco | Software Engineer' userImg='/assets/images/dummy.jpg' />
-            <h1 className='text-2xl font-karla'>User ID: {id}</h1>
+        <div className="wrapper page">
+            <Header
+                subHeader="marco@email.com"
+                title="Marco | Software Engineer"
+                userImg="/assets/images/dummy.jpg"
+            />
+            <section className="video-grid">
+                {dummyCards.map((card) => (
+                    <VideoCard key={card.id} {...card} />
+                ))}
+            </section>
         </div>
-    )
-}
+    );
+};
 
-export default page
+export default page;

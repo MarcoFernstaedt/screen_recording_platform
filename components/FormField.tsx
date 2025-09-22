35802f45dba98ad13eef624fa435d590a3456f0d
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-09-22 16:05:04 -0700

    fix input render

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -12,25 +12,25 @@ const FormField = ({
 }: FormFieldProps) => {
 
     const InputToRender = ({ type }: { type: "string" }) => {
-        if (type === "textarea") {
-            return <textarea id={id} name={id} value={value} onChange={onChange} placeholder={placeholder} />;
-        } else if (type === "select") {
-            return <select id={id} name={id} value={value} onChange={onChange}>
-                {options.map(({ value, label }) => (
+        {
+            as === 'textarea' ? (
+                <textarea id={id} name={id} value={value} onChange={onChange} placeholder={placeholder} />
+            ) : as === 'select' ? (
+                <select id={id} name={id} value={value} onChange={onChange}>
                     <option key={value}>{label}</option>
-                ))}
-            </select>
-        } else {
-            return <input id={id} name={id} value={value} onChange={onChange} placeholder={placeholder} />;
+                </select>
+            ) : (
+                <input id={id} name={id} value={value} onChange={onChange} placeholder={placeholder} />
+            )
         }
+
+
+        return (
+            <div className="form-field">
+                <label htmlFor={id}>{label}</label>
+                <InputToRender type={as} />
+            </div>
+        );
     };
 
-    return (
-        <div className="form-field">
-            <label htmlFor={id}>{label}</label>
-            <InputToRender type={as} />
-        </div>
-    );
-};
-
-export default FormField;
+    export default FormField;

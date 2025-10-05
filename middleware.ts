d2f0489d6ad3e501a3a8964e4247218459ff385c
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-10-05 12:32:47 -0700

    implament arcjet rate limiting and email validation in m auth bot detection and shield implamented in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "./lib/auth";
 import { headers } from "next/headers";
+import aj from "./lib/arcjet";
+import { createMiddleware, detectBot, shield } from "@arcjet/next";
 
-export const middleware = async ( request: NextRequest, response: NextResponse) => {
-    const session = await auth.api.getSession({
-        headers: await headers()
+export const middleware = async (
+  request: NextRequest,
+  response: NextResponse
+) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+};
+
+const validate = aj
+  .withRule(shield({ mode: "LIVE" }))
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "GOOGLE_CRAWLER"],
     })
+  );
 
-    if (!session) {
-        return NextResponse.redirect(new URL('/sign-in', request.url))
-    }
-
-    return NextResponse.next()
-}
+export default createMiddleware(validate);
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)']
-}
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)"],
+};

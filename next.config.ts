ba6e38f8a424924131fc377186f8434150420875
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-10-05 14:19:22 -0700

    config images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: 'loom-a-like.b-cdn.netmain', protocol: 'https', port: '', pathname: '/**'},
+      { hostname: 'lh3.googleusercontent.com', protocol: 'https', port: '', pathname: '/**'},
+    ]
+  }
 };
 
 export default nextConfig;
